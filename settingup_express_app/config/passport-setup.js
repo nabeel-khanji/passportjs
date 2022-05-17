d1ccfa5a8ed39f6599f356ade689d6a1ff1ad210
Muhammad Nabeel <muhammad.nabeel@cubixlabs.com>
2022-05-17 22:13:59 +0500

    google keys

--- settingup_express_app/config/passport-setup.js
+++ settingup_express_app/config/passport-setup.js
@@ -2,6 +2,9 @@ const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20");
 passport.use(
   new GoogleStrategy({
+    clientID:
+      "447852305885-5od3gpd65f1p8tmv5ectbj2ht53t8nrr.apps.googleusercontent.com",
+    clientSecret: "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",
     //option for google strat
   }),
   () => {
