5353acd50354749081282c03f97d4da307a0aa18
Muhammad Nabeel <muhammad.nabeel@cubixlabs.com>
2022-05-17 21:11:56 +0500

    express setup

---
+++ settingup_express_app/src/app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const hbs= require("hbs");
+const router = require("../routes/routes");
+const path = require("path");
+const app = express();
+const port = process.env.PORT || 8000;
+const view_path = path.join(__dirname, "../template/view");
+const partial_path = path.join(__dirname, "../template/partials");
+const public_path = path.join(__dirname, "../public");
+app.set("view engine", "hbs");
+app.set("views", view_path);
+app.use(express.static(public_path));
+app.use(router);
+hbs.registerPartials(partial_path);
+app.listen(port, () => {
+  console.log(`App listening on port ${port}!`);
+});
