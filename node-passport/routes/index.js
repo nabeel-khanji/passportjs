00bd55aaffb4e2dacc5fdc24f5136e11129b4854
Muhammad Nabeel <muhammad.nabeel@cubixlabs.com>
2022-05-27 19:07:14 +0500

    index

--- node-passport/routes/index.js
+++ node-passport/routes/index.js
@@ -1,15 +1,26 @@
 const express = require("express");
 const router = express.Router();
 const { ensureAuthenticated, forwardAuthenticated } = require("../config/auth");
+const Role = require("../model/Role");
+const Route = require("../model/Route");
 // Welcome Page
 router.get("/", forwardAuthenticated, (req, res) => {
   res.render("welcome");
 });
 // Dashboard Page
 router.get("/dashboard", ensureAuthenticated, (req, res) => {
-  res.render("dashboard", {
-    role: req.user.role,
-    name: req.user.name,
+  console.log(req.user);
+  Role.findOne({ _id: req.user.role }).then((role) => {
+    Route.find({}).then(route=>{
+  
+      res.render("dashboard", {  
+        layout:"LayoutA",
+        role: role.permissions,
+        name: req.user.name,
+        route:route
+      });
+    }).catch(err=>console.log(err));
+   
   });
 });
 
